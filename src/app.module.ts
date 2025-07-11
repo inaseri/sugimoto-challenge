8aa515b451a2e27565455d1c17d14f3c71ad2b2d
Iman <inaseri.20@gmail.com>
2025-07-11 13:04:43 +0330

    feat: Add auth module base struct

--- src/app.module.ts
+++ src/app.module.ts
@@ -3,6 +3,7 @@ import { AppController } from './app.controller';
 import { AppService } from './app.service';
 import { ConfigModule } from '@nestjs/config';
 import { SupabaseModule } from './shared/supabase/supabase.module';
+import { AuthModule } from './auth/auth.module';
 
 @Module({
   imports: [
@@ -11,6 +12,7 @@ import { SupabaseModule } from './shared/supabase/supabase.module';
       isGlobal: true,
     }),
     SupabaseModule,
+    AuthModule,
   ],
   controllers: [AppController],
   providers: [AppService],
